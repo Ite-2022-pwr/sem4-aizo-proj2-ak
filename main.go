4b76315b2697a40f806ac5da551ba698b2535fd6
arkregiel <arkregiel@gmail.com>
2024-06-01 13:51:47 +0200

    add benchmarks for dijkstra

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "github.com/Ite-2022-pwr/sem4-aizo-proj2-ak/benchmark"
 
 func main() {
 
-  // benchmark.BenchmarkDijsktra()
+  benchmark.BenchmarkDijsktra()
   // benchmark.BenchmarkBellmanFord()
   // benchmark.BenchmarkKruskal()
-  benchmark.BenchmarkPrim()
+  // benchmark.BenchmarkPrim()
 }
